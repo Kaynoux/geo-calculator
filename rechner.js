da6642cfced70f69531e6e290bc951d5d9694df0
Lukas <58143386+Kaynoux@users.noreply.github.com>
2024-07-09 19:55:03 +0200

    New Stuff

--- rechner.js
+++ rechner.js
@@ -1,66 +1,134 @@
-var divA = document.getElementById('a');
-var divB = document.getElementById('b');
-var divC = document.getElementById('c');
-var divR = document.getElementById('r');
-var divArea = document.getElementById('area');
+let shapeSelect, divA, divB, divC, divR, divG, divH, divArea, divVolume;
+let inputA, inputB, inputC, inputR, inputG, inputH, inputArea, inputVolume;
+var currentShape = "0";
 
-var inputA = document.getElementById('input-a');
-var inputB = document.getElementById('input-b');
-var inputC = document.getElementById('input-c');
-var inputR = document.getElementById('input-r');
-var inputArea = document.getElementById('input-area');
+document.addEventListener('DOMContentLoaded', () => {
+  const shapeSelect = document.getElementById('2d-shape-select');
 
-// Triangle is default value
-var currentShape = 0;
-divA.style.display = "block";
-divB.style.display = "block";
-divC.style.display = "block";
-divR.style.display = "none";
+  divA = document.getElementById('a');
+  divB = document.getElementById('b');
+  divC = document.getElementById('c');
+  divR = document.getElementById('r');
+  divG = document.getElementById('g');
+  divH = document.getElementById('h');
+  divGArea = document.getElementById('g-area');
+  divArea = document.getElementById('area');
+  divVolume = document.getElementById('volume');
 
-document.getElementById('2d-shape-select').addEventListener('change', 
-function formChange2d() {   
-  currentShape = this.value;
+  inputA = document.getElementById('input-a');
+  inputB = document.getElementById('input-b');
+  inputC = document.getElementById('input-c');
+  inputR = document.getElementById('input-r');
+  inputG = document.getElementById('input-g');
+  inputH = document.getElementById('input-h');
+  inputGArea = document.getElementById('input-g-area');
+  inputArea = document.getElementById('input-area');
+  inputVolume = document.getElementById('input-volume');
+
+  updateFields();
+  shapeSelect.addEventListener('change', updateFields);
+
+  function updateFields() {
+    currentShape = shapeSelect.value;
+
+    // Alle Felder ausblenden
+    divA.style.display = 'none';
+    divB.style.display = 'none';
+    divC.style.display = 'none';
+    divR.style.display = 'none';
+    divG.style.display = 'none';
+    divH.style.display = 'none';
+    divGArea.style.display = 'none';
+    divArea.style.display = 'none';
+    divVolume.style.display = 'none';
+    
+    inputA.value = '';
+    inputB.value = '';
+    inputC.value = '';
+    inputR.value = '';
+    inputG.value = '';
+    inputH.value = '';
+    inputGArea.value = '';
+    inputArea.value = '';
+    inputVolume.value = '';
+
+    // Relevante Felder einblenden
+    switch (currentShape) {
+      case 'triangle':
+        divA.style.display = 'block';
+        divB.style.display = 'block';
+        divC.style.display = 'block';
+        divArea.style.display = 'block';
+        break;
+      case 'square':
+        divA.style.display = 'block';
+        divArea.style.display = 'block';
+        break;
+      case 'rectangle':
+        divA.style.display = 'block';
+        divB.style.display = 'block';
+        divArea.style.display = 'block';
+        break;
+      case 'circle':
+        divR.style.display = 'block';
+        divArea.style.display = 'block';
+        break;
+      case 'cube':
+        divA.style.display = 'block';
+        divVolume.style.display = 'block';
+        break;
+      case 'cuboid':
+        divA.style.display = 'block';
+        divB.style.display = 'block';
+        divC.style.display = 'block';
+        divVolume.style.display = 'block';
+        break;
+      case 'sphere':
+        divR.style.display = 'block';
+        divVolume.style.display = 'block';
+        break;
+      case 'pyramid3':
+        divGArea.style.display = 'block';
+        divH.style.display = 'block';
+        divVolume.style.display = 'block';
+        break;
+      case 'pyramid4':
+        divGArea.style.display = 'block';
+        divH.style.display = 'block';
+        divVolume.style.display = 'block';
+    }
+  }
+});
+
+function calculate() {
+  console.log("Calcuy^late" + currentShape);
   switch (currentShape) {
-    case "0":
-      divA.style.display = "block";
-      divB.style.display = "block";
-      divC.style.display = "block";
-      divR.style.display = "none";
-      break;
-    case "1":
-      divA.style.display = "block";
-      divB.style.display = "none";
-      divC.style.display = "none";
-      divR.style.display = "none";
-      break;
-    case "2":
-      divA.style.display = "block";
-      divB.style.display = "block";
-      divC.style.display = "none";
-      divR.style.display = "none";
-      break;
-    case "3":
-      divA.style.display = "none";
-      divB.style.display = "none";
-      divC.style.display = "none";
-      divR.style.display = "block";
-      break;
-  } 
-}
-);
-
-function calculate2d() {
-  console.log("Calculate" + currentShape);
-  switch (currentShape) {
-    case "0":
+    case "triangle":
       calculateTriangle();
       break;
-    case "1":
+    case "square":
       calculateSquare();
       break;
-    case "2":
+    case "rectangle":
+      calculateRectangle();
       break;
-    case "3":
+    case "circle":
+      calculateCircle();
+      break;
+    case "cube":
+      calculateCube();
+      break;
+    case "cuboid":
+      calculateCuboid();
+      break;
+    case "sphere":
+      calculateSphere();
+      break;
+    case "pyramid3":
+      calculatePyramid();
+      break;
+    case "pyramid4":
+      calculatePyramid();
       break;
   } 
 }
@@ -102,8 +170,8 @@ function calculateTriangle()
 
 function calculateSquare()
 {
-  var a = parseFloat(inputA.value);
-  var area = parseFloat(inputArea.value);
+  let a = parseFloat(inputA.value);
+  let area = parseFloat(inputArea.value);
 
   console.log(a);
   console.log(area);
@@ -120,4 +188,135 @@ function calculateSquare()
   {
     alert("Mindestens 1 Wert eingeben");
   }
+}
+
+function calculateRectangle()
+{
+  let a = parseFloat(inputA.value);
+  let b = parseFloat(inputB.value);
+  let area = parseFloat(inputArea.value);
+
+  if(a && b)
+  {
+    inputArea.value = a * b;
+  }
+  else if(a && area)
+  {
+    inputB.value = area / a;
+  }
+  else if(b && area)
+  {
+    inputA.value = area / b;
+  }
+  else
+  {
+    alert("Mindestens 2 Werte eingeben");
+  }
+}
+
+function calculateCircle()
+{
+  let r = parseFloat(inputR.value);
+  let area = parseFloat(inputArea.value);
+
+  if(r)
+  {
+    inputArea.value = Math.PI * r * r;
+  }
+  else if(area)
+  {
+    inputR.value = Math.sqrt(area / Math.PI);
+  }
+}
+
+function calculateCube()
+{
+  let a = parseFloat(inputA.value);
+  let volume = parseFloat(inputVolume.value);
+
+  if(a)
+  {
+    inputVolume.value = a * a * a;
+  }
+  else if(volume)
+  {
+    inputA.value = Math.cbrt(volume);
+  }
+  else
+  {
+    alert("Mindestens 1 Wert eingeben");
+  }
+
+}
+
+function calculateCuboid()
+{
+  let a = parseFloat(inputA.value);
+  let b = parseFloat(inputB.value);
+  let c = parseFloat(inputC.value);
+  let volume = parseFloat(inputVolume.value);
+
+  if (a && b && c)
+  {
+    inputVolume.value = a * b * c;
+  }
+  else if (a && b && volume)
+  {
+    inputC.value = volume / (a * b);
+  }
+  else if (a && c && volume)
+  {
+    inputB.value = volume / (a * c);
+  }
+  else if (b && c && volume)
+  {
+    inputA.value = volume / (b * c);
+  }
+  else
+  {
+    alert("Mindestens 3 Werte eingeben");
+  }
+}
+
+function calculateSphere()
+{
+  let r = parseFloat(inputR.value);
+  let volume = parseFloat(inputVolume.value);
+
+  if(r)
+  {
+    inputVolume.value = 4 / 3 * Math.PI * r * r * r;
+  }
+  else if(volume)
+  {
+    inputR.value = Math.cbrt(volume / (4 / 3 * Math.PI));
+  }
+  else 
+  {
+    alert("Mindestens 1 Wert eingeben");
+  }
+}
+
+function calculatePyramid()
+{
+  let g = parseFloat(inputGArea.value);
+  let h = parseFloat(inputH.value);
+  let volume = parseFloat(inputVolume.value);
+
+  if(g && h)
+  {
+    inputVolume.value = 1 / 3 * g * g * h;
+  }
+  else if(g && volume)
+  {
+    inputH.value = volume / (1 / 3 * g * g);
+  }
+  else if(h && volume)
+  {
+    inputG.value = Math.sqrt(volume / (1 / 3 * h));
+  }
+  else
+  {
+    alert("Mindestens 2 Werte eingeben");
+  } 
 }
